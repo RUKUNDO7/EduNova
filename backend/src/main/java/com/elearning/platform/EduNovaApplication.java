ef4917039be8bab66c896d8b6674a06b45d12560
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-18 21:29:56 +0200

    Added backend entry point

---
+++ backend/src/main/java/com/elearning/platform/EduNovaApplication.java
@@ -0,0 +1,11 @@
+package com.elearning.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduNovaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EduNovaApplication.class, args);
+    }
+}
